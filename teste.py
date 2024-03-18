6003d2208c9fbdd61584dfa9b0a3921f6d8686cb
geleiacompepino <brthy40@gmail.com>
2024-03-18 00:10:26 -0300

    Fix gitignore and test wav2vec

--- teste.py
+++ teste.py
@@ -1,3 +1,17 @@
-import fairseq
-ckpt_path = "D:/Downloads/GPTSoVits/GPT-SoVITS/GPT_SoVITS/pretrained_models/hubert_large_ll60k.pt"
-models, cfg, task = fairseq.checkpoint_utils.load_model_ensemble_and_task([ckpt_path])
+import librosa
+import torch
+from transformers import Wav2Vec2ForCTC, Wav2Vec2Processor
+AUDIO_FILE = 'teste.wav'
+processor = Wav2Vec2Processor.from_pretrained("lgris/wav2vec2-large-xlsr-open-brazilian-portuguese")
+model = Wav2Vec2ForCTC.from_pretrained("lgris/wav2vec2-large-xlsr-open-brazilian-portuguese")
+speech, rate = librosa.load(AUDIO_FILE, sr=16000)
+ 
+# Tokenizing the input
+inputs = processor(speech, return_tensors="pt", sampling_rate=rate).input_values
+ 
+# Model logits
+with torch.no_grad():
+    logits = model(inputs).logits
+predicted_ids = torch.argmax(logits, dim=-1)
+transcription = processor.batch_decode(predicted_ids)
+print(transcription)
